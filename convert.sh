f50ae1e12cc625e59d1a25e10f8dca53b913d95a
nayanasri2026-art <nayanasri2026@gmail.com>
2026-08-12 18:17:56 +0530

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,8 +1,8 @@
 # Change input and output details
 
-output_path="big-buck-bunny" # Output Path (Same will be pushed in main also)
-input_url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" # Input direct file url
-input_extension="mp4" # Extension of file url
+output_path="oh" # Output Path (Same will be pushed in main also)
+input_url="https://cdn.juicybits.site/files/www.1TamilMV.Li_-_Oh.._Sukumari_2026_HQ_HDRip_-_1080p_-_x264_-_Malayalam__Kannada_-_DD5.1_-_640Kbps_-_3GB_-_ESub.mkv?token=35e31b2e084531b1401fcafb8793865c&exp=1786553223" # Input direct file url
+input_extension="mkv" # Extension of file url
 
 
 
@@ -12,8 +12,6 @@ wget --quiet -O video.$input_extension $input_url
 mkdir $output_path
 
 ffmpeg -hide_banner -y -i video.$input_extension \
-  -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename $output_path/360p_%03d.ts $output_path/360p.m3u8 \
-  -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename $output_path/480p_%03d.ts $output_path/480p.m3u8 \
   -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename $output_path/720p_%03d.ts $output_path/720p.m3u8 \
   -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 5000k -maxrate 5350k -bufsize 7500k -b:a 192k -hls_segment_filename $output_path/1080p_%03d.ts $output_path/1080p.m3u8
 
@@ -22,10 +20,6 @@ cd $output_path
 
 echo '#EXTM3U
 #EXT-X-VERSION:3
-#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x360
-360p.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480
-480p.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720
 720p.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080
